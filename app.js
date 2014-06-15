b8af944f7d5d4c81d934132f9554e431adb300b3
Norwill Gutierrez <gutierreznorwill@gmail.com>
2014-06-15 15:14:38 -0430

    Adding files

---
+++ app.js
@@ -0,0 +1,16 @@
+var app = angular.module('MyTutorialApp',[]);
+
+angular.module('MyTutorialApp', [])
+
+.directive('blurred', function () {
+  var directive = { restrict: 'A' };
+  directive.compile = function compile (tElement) {
+    // taken from blur.js homepage
+    tElement.blurjs({
+      source: '#bgToBlurContainer',
+      radius: 10,
+      overlay: 'rgba(0,0,0,0.5)'
+    });
+  };
+  return directive;
+});
